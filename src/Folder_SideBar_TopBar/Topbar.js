72ef67bac6b352d5896572791434805f8ef3d5d9
Nicolas <nicolas@MBP-de-Nicolas.lan>
2021-09-28 20:17:48 -0600

    Added little things

--- src/Folder_SideBar_TopBar/Topbar.js
+++ src/Folder_SideBar_TopBar/Topbar.js
@@ -1,20 +1,28 @@
 import React,{useContext, useState} from 'react'
 
 const Topbar = () => {
+    const nombre_empresa = "The CheckPoint"
+    const usuario={name:"Juanito", cargo: "Administrador"}
 
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
-            <div className="container px-5">
-                <div className="nombre_empresa border border-primary w-75 d-flex justify-content-center">
-                    The CheckPoint
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3 p-0 w-100">
+            <div className="container">
+                <div className="nombre_empresa">
+                    <h3 >{nombre_empresa}</h3>
                 </div>
-                <div className="usuario border border-primary">
-                    <div className="nombre_usuario">
-                        Admin
+                <div className="usuario_global d-sm-none d-md-flex" > 
+                    <div className="datos_usuario me-4 d-flex flex-column my-auto">
+                        <div className="nombre_usuario mx-auto">
+                            {usuario.name}
+                        </div>
+                        <div className="permiso_usuario mx-auto">
+                            <small className="text-muted">{usuario.cargo}</small>
+                            
+                        </div>
                     </div>
                     <div className="foto_usuario">
-                        
+                        <i class="fas fa-users fa-4x"></i>
                     </div>
                 </div>
             </div>
