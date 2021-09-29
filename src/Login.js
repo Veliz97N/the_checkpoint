674a1ae0cf82958b5504e9f7e4a6572709c17d22
Joaquin Abarca <joaquinabarca@live.com>
2021-09-29 00:00:18 -0300

    added css variables for themes

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,27 @@
 function Login() {
   return (
+    // contenedor principal
     <div className="container">
+      {/* contenedor para login  */}
       <div className="login">
         <h1>Bienvenido</h1>
         <h6>Por favor inicia sesión para continuar</h6>
       </div>
+      {/* Input username */}
       <div className="username-input">
       <input type="text" placeholder="Usuario" name="" id="" />
       </div>
+      {/* Input password */}
       <div className="password-input">
       <input type="text" placeholder="Contraseña" name="" id="" />
       </div>
+      {/* Login button */}
       <div className="login-button">
       <button className="btn btn-success" type="button">
         Iniciar sesión
       </button>
       </div>
+      {/* Connect facebook y google buttons */}
       <div className="connect-buttons">
       <button className="btn btn-primary btn-sm fb" type="button">
         Conectar con Facebook
