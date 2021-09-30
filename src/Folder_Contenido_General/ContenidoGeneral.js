53c4657304727ff629c1e7906344e7761485525e
Nicolas <nicolas@MBP-de-Nicolas.lan>
2021-09-30 08:06:01 -0600

    Se guarda esta version

--- src/Folder_Contenido_General/ContenidoGeneral.js
+++ src/Folder_Contenido_General/ContenidoGeneral.js
@@ -27,7 +27,7 @@ const ContenidoGeneral = () => {
           <Sidebar></Sidebar>
           <Switch>
             <div className="container-fluid" style={!isDesplegado ? activo : no_activo}>
-
+        
             <Route exact path="/Catalogo_IngresarNuevoProducto" component={IngresarNuevoProducto} />
 
             
