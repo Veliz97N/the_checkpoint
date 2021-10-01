18bcf69a504afde4bbd3f29a5068add744bce2b9
Nicolas <nicolas@MBP-de-Nicolas.lan>
2021-10-01 10:49:04 -0600

    Se integra filtrado de datos

--- src/UserContext/UserContext.js
+++ src/UserContext/UserContext.js
@@ -3,10 +3,11 @@ import React, { createContext, useState } from 'react';
 const UserContext = createContext();
 
 const usuario = { username: "Juan Carlos", password: "Juanito1", permiso: "Administrador", tema: "Dark", Fuente: { tipo: "Arial", tamano: 48, titulo_sidebar: true }, isFacebook: false, isGoogle: false }
-const productos=[{nombreProducto:"Papas Fritas",codigodebarras:"123123123123",categoria:"Abarrotes",valorUnidad:"1100",stockDisponible:"82"},
-                {nombreProducto:"Papas Fritas",codigodebarras:"123123123123",categoria:"Abarrotes",valorUnidad:"1100",stockDisponible:"82"},
-                {nombreProducto:"Papas Fritas",codigodebarras:"123123123123",categoria:"Abarrotes",valorUnidad:"1100",stockDisponible:"82"},
-                {nombreProducto:"Papas Fritas",codigodebarras:"123123123123",categoria:"Abarrotes",valorUnidad:"1100",stockDisponible:"82"}]
+
+const productos=[{nombreProducto:"Modelo",codigodebarras:"8888888888",categoria:"Abarrotes",valorUnidad:"1100",stockDisponible:"82"},
+                {nombreProducto:"Corona",codigodebarras:"555555555555",categoria:"Abarrotes",valorUnidad:"1100",stockDisponible:"82"},
+                {nombreProducto:"Papas Fritas",codigodebarras:"444444444",categoria:"Abarrotes",valorUnidad:"1100",stockDisponible:"82"},
+                {nombreProducto:"Chocman",codigodebarras:"12312313",categoria:"Abarrotes",valorUnidad:"1100",stockDisponible:"82"}]
 //Se pasa usuario, productos como variable global para simular la obtencion de la informacion proveniente de la base de datos
 
 const UserProvider = ({ children }) => {
