9256b9fed7a07b43cfb049a97ad3b8ba9ed21d67
Nicolas <nicolas@MBP-de-Nicolas.lan>
2021-10-01 11:31:32 -0600

    Se agrega dropdown a tarjeta_usuario

--- src/Folder_SideBar_TopBar/Tarjeta_Usuario_Activo.js
+++ src/Folder_SideBar_TopBar/Tarjeta_Usuario_Activo.js
@@ -18,32 +18,50 @@ const Tarjeta_Usuario_Activo = (props) => {
         justifyContent:'center', 
         transition: "all ease .5s", 
       };
+      const estilo_dropdownmenuNuevo ={
+          zIndex: '2000'
+      }
 
       const [isHover, setIsHover] = useState(null)
 
     return (
+        
 
-            <div style={isHover ? activo_tarjeta_usuario : no_activo_tarjeta_usuario}
-                className="usuario d-sm-none d-md-flex"
-                onMouseOver={() => setIsHover(true)}
-                onMouseOut={() => setIsHover(false)}>
-
-                <div className="datos_usuario me-4 d-flex flex-column my-auto">
-                    <div className="nombre_usuario mx-auto">
-                        {props.user.username}
-                    </div>
-
-                    <div className="permiso_usuario mx-auto">
-                        <small className="text-white">{props.user.permiso}</small>
-                    </div>
-
+        <div style={isHover ? activo_tarjeta_usuario : no_activo_tarjeta_usuario}
+            className="usuario btn-group d-sm-none d-md-flex"
+            onMouseOver={() => setIsHover(true)}
+            onMouseOut={() => setIsHover(false)}>
+            
+            <button class="btn dropdown-toggle d-md-flex" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            
+            <div className="datos_usuario me-4 d-flex flex-column my-auto">
+                <div className="nombre_usuario mx-auto">
+                    {props.user.username}
                 </div>
 
-                <div className="foto_usuario">
-                    <i class="fas fa-users fa-3x"></i>
+                <div className="permiso_usuario mx-auto">
+                    <small className="text-white">{props.user.permiso}</small>
                 </div>
 
             </div>
+
+            <div className="foto_usuario">
+                <i class="fas fa-users fa-3x"></i>
+                <div>
+                {/* <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    ⬇
+                </button> */}
+                
+            </div>
+            </div>
+            
+            </button>
+            <div style={estilo_dropdownmenuNuevo} class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Cerrar Sesion</a>
+                    <a class="dropdown-item" href="#">Crear Usuario</a>
+                    <a class="dropdown-item" href="#">Modificar Usuario</a>
+                </div>
+        </div>
 
     )
 }
