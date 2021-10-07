56b4d430d0a08268534c1679c2ac416fad1151c1
Joaquin Abarca <joaquinabarca@live.com>
2021-10-06 23:13:22 -0300

    catalogo_pagprincipal bueno(?

--- src/Vistas/Catalogo/Catalogo_PaginaPrincipal.js
+++ src/Vistas/Catalogo/Catalogo_PaginaPrincipal.js
@@ -1,83 +1,45 @@
-import React, { useState, useContext } from "react";
+import React,{useState,useContext} from 'react'
 import { AiOutlineDelete, AiFillDelete } from "react-icons/ai";
 import { GiConfirmed } from "react-icons/gi";
-import Producto_en_tabla_catalogo from "./Producto_en_tabla_catalogo";
-import UserContext from "../../UserContext/UserContext";
+import Producto_en_tabla_catalogo from './Producto_en_tabla_catalogo';
+import UserContext from '../../UserContext/UserContext';
 import { Link } from "react-router-dom";
 import { IoAddCircleOutline, IoAddCircleSharp } from "react-icons/io5";
 
-import Layout from "../../Folder_Contenido_General/Layout";
+import Layout from '../../Folder_Contenido_General/Layout';
 
 const Catalogo_PaginaPrincipal = () => {
-  const { productos } = useContext(UserContext);
-  const noactivopapi = {
-    background: "#57CC99",
-    transition: "all 0.5s ease;",
-  };
-  const Catalogo_PaginaPrincipal = {
-    position: "relative",
-  };
-  const contenedor_tabla = {
-    position: "absolute",
-    width: "100%",
-  };
-  const botonIngresarNuevoProducto = {
-    position: "absolute",
-    top: "25px",
-    right: "25px",
-    zIndex: "1000",
-    background: "#80ED99",
-    borderRadius: "25px",
-    color: "black",
-    textDecoration: "none",
-  };
-  const botonagregarNuevoProducto = {
-    fontSize: "1.4rem",
-  };
+    const { productos } = useContext(UserContext);
+    const noactivopapi = {
+        background: "#57CC99",
+        transition: "all 0.5s ease;",
+    }
+    const Catalogo_PaginaPrincipal = {
+        position: 'relative',
+    }
+    const contenedor_tabla = {
+        position: 'absolute',
+        width: '100%'
+    }
+    const botonIngresarNuevoProducto = {
+        position: 'absolute',
+        top: '25px',
+        right: '25px',
+        zIndex: '1000',
+        background: '#80ED99',
+        borderRadius: '25px',
+        color: 'black',
+        textDecoration: 'none',
+    }
+    const botonagregarNuevoProducto = {
+        fontSize: '1.4rem'
+    }
 
-  const [indiceBuscarElemento, setIndiceBuscarElemento] = useState("0");
+    const [indiceBuscarElemento, setIndiceBuscarElemento] = useState("0")
 
-  const handleAddrTypeChange = (e) => {
-    setIndiceBuscarElemento(e.target.value);
-  };
-  const [addrtype, setAddrtype] = useState([
-    "Buscar por Nombre",
-    "Buscar por Categoria",
-    "Buscar por Codigo de Barras",
-  ]);
-  const Add = addrtype.map((Add) => Add);
-
-  const [listaProductosFiltrado, setListaProductosFiltrado] = useState([]);
-
-  const [valor, setValor] = useState("");
-
-  const handlesetvalor = (e) => {
-    setValor(e.target.value);
-  };
-
-  const funcion_filtrar_busqueda_producto = (tipoBusqueda, valorBusqueda) => {
-    console.log(valorBusqueda);
-    if (tipoBusqueda === "0" && valorBusqueda !== "") {
-      const productoFiltrados = productos.filter((producto) =>
-        producto.nombreProducto
-          .toLowerCase()
-          .includes(valorBusqueda.toLowerCase())
-      );
-      setListaProductosFiltrado(productoFiltrados);
-    } else if (tipoBusqueda === "1" && valorBusqueda !== "") {
-      const productoFiltrados = productos.filter((producto) =>
-        producto.categoria.toLowerCase().includes(valorBusqueda.toLowerCase())
-      );
-      setListaProductosFiltrado(productoFiltrados);
-    } else if (tipoBusqueda === "2" && valorBusqueda !== "") {
-      const productoFiltrados = productos.filter((producto) =>
-        producto.codigodebarras.includes(valorBusqueda)
-      );
-      setListaProductosFiltrado(productoFiltrados);
-    } else if (valorBusqueda === "" || listaProductosFiltrado.length === 0) {
-      //Si el valor de busqueda es ''
-      const productoFiltrados = [];
-      setListaProductosFiltrado(productoFiltrados);
+    const handleAddrTypeChange = (e) => {
+        setIndiceBuscarElemento(e.target.value)
+        
     }
     const [addrtype, setAddrtype] = useState(["Buscar por Nombre", "Buscar por Categoria", "Buscar por Codigo de Barras"])
     const Add = addrtype.map(Add => Add
@@ -164,36 +126,16 @@ const Catalogo_PaginaPrincipal = () => {
                                     />
                             </td>
 
-                <td colspan="4">
-                  <input
-                    type="text"
-                    className="form-control"
-                    onChange={handlesetvalor}
-                    onKeyPress={(e) => {
-                      if (e.key === "Enter") {
-                        funcion_filtrar_busqueda_producto(
-                          indiceBuscarElemento,
-                          valor
-                        );
-                      }
-                    }}
-                  />
-                </td>
-              </tr>
-              {listaProductosFiltrado.length >= 1
-                ? listaProductosFiltrado.map((producto) => (
-                    <Producto_en_tabla_catalogo producto={producto} />
-                  ))
-                : productos.map((producto) => (
-                    <Producto_en_tabla_catalogo producto={producto} />
-                  ))}
-            </tbody>
-          </table>
+                        </tr>
+                        {   (listaProductosFiltrado.length >=1 )?listaProductosFiltrado.map(producto =>  <Producto_en_tabla_catalogo  producto={producto}/>)
+                        :productos.map(producto =>  <Producto_en_tabla_catalogo  producto={producto}/>)}                        
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
-    </Layout>
-  );
-};
+        </Layout>
+    )
 }
 
-export default Catalogo_PaginaPrincipal;
+export default Catalogo_PaginaPrincipal
