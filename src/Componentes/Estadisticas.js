1cf954bc8b959235733d73e28dab43c9cca2aa96
Joaquin Abarca <joaquinabarca@live.com>
2021-10-06 23:03:57 -0300

    blabla

--- src/Componentes/Estadisticas.js
+++ src/Componentes/Estadisticas.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
+import { useState } from 'react/cjs/react.development';
 import Layout from '../Folder_Contenido_General/Layout'
 
 function Estadisticas(){
@@ -8,7 +9,9 @@ function Estadisticas(){
 
     const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 
-    const data1 = {
+    const datosRecibidos = [10000, 515, 651, 239, 658, 557, 758]
+
+    const informacion_Base_Datos = {
         labels: dias,
         datasets:[{
             label: "Venta Semanal",
@@ -17,19 +20,19 @@ function Estadisticas(){
             hoverBackgroundColor: '#4B0082',
             data: [435, 515, 651, 239, 658, 557, 758]
         }]
-    
     }
-   /*  const data2 = {
-        labels: meses,
-        datasets:[{
-            label: "Venta Mensual",
-            backgroundColor: '#9400D3',
-            borderWidth: 1,
-            hoverBackgroundColor: '#4B0082',
-            data2: [435, 515, 651, 239, 658, 557, 758, 651, 239, 658, 557, 758]
-        }]
-    
-    } */
+  
+    const [datos_A_Graficar, setDatos_A_Graficar] = useState(datosRecibidos)
+
+
+
+
+    const funcionObtenerInformacionBaseDatos = () =>{
+
+
+
+
+    }
 
 
     const opciones={
@@ -40,9 +43,40 @@ function Estadisticas(){
     return (
         <Layout hasNavbar hasSidebar>
         <div className="container">
-            <div className="row">
-                <div className="col-4 d-flex justify-content-center aling-items-center" id="stadistics">
-                    Estadisticas
+          <div className="row mb-3">
+            <div className="col-12 d-flex justify-content-center aling-items-center">
+              <div
+                className="col-4 d-flex justify-content-center aling-items-center"
+                id="stadistics"
+              >
+                Estadisticas
+              </div>
+            </div>
+          </div>
+
+          <div className="row " id="tabla1">
+            <div className="col-md-6 col-sm-12 px-1 mb-3">
+              <div className="prueba" id="col1">
+                <div
+                  className="d-flex justify-content-center"
+                  id="titleStadistic"
+                >
+                  <select
+                  
+                  className="browser-default custom-select col-4 me-2"
+                >
+                 
+                    <option>
+                      Venta Diaria
+                    </option>
+                    <option>
+                      Venta Semanal
+                    </option>
+                    <option>
+                      Venta Mensual
+                    </option>
+                  
+                </select>
                 </div>
             </div>
             <div className="row align-items-start" id="tabla1">
@@ -82,7 +116,7 @@ function Estadisticas(){
                         Grafico de ventas
                     </div>
                     <div className=" col d-flex aling-content-center" id="datas">
-                    <Bar data={data1} options={opciones} />
+                    <Bar data={datosRecibidos} options={opciones} />
                     </div>
                 {/*     <div className=" col d-flex justify-content-center" id="titleStadistic">
                     <Bar data={data2} options={opciones} />
@@ -131,7 +165,57 @@ function Estadisticas(){
                         </tbody>
                     </table>
                 </div>
+                <div className=" col d-flex aling-content-center" id="datas">
+                  <Bar data={datos_A_Graficar} options={opciones} />
+                </div>
+              </div>
             </div>
+          </div>
+        </div>
+
+        <div className="row mt-1 p-2" id="tabla2">
+          <div className="col-12">
+            <div className="prueba " id="col3">
+              <div className="d-flex justify-content-center" id="col2">
+                Listado de Productos Mas Vendidos
+              </div>
+
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Nro</th>
+                    <th scope="col">Producto</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Porcentaje de Venta</th>
+                    <th scope="col">Stock</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Vienesa</td>
+                    <td>Cecinas</td>
+                    <td>100</td>
+                    <td>100</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Pan</td>
+                    <td>Abarrotes</td>
+                    <td>100</td>
+                    <td>100</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Danky</td>
+                    <td>Helados</td>
+                    <td>100</td>
+                    <td>100</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+        </div>
         </div>
     </Layout>
     )
